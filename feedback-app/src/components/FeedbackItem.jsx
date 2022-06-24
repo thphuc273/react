c3e2bdda6b0e6dc3d8fafc42419f7e6d4c2530eb
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-24 14:55:43 +0700

    event & prop drilling

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,9 +1,16 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
+  const handleClick = () => {
+
+  }
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+          <FaTimes color = 'purple'></FaTimes>
+        </button>
         <div className="text-display">{item.text}</div>
     </Card>
   )
