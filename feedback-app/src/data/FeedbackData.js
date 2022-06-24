5f3d52e436c31fdd7dadceed9664f9b240174058
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-24 14:23:05 +0700

    Card Componet & Conditional Style

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 8,
+        text: 'Good app!! Nice to use'
+      },
+      {
+        id: 2,
+        rating: 9,
+        text: 'Excellent!!'
+      },
+      {
+        id: 3,
+        rating: 7,
+        text: 'Very good'
+      },
+      {
+        id: 4,
+        rating: 6,
+        text: 'It\'s OK'
+      }
+]
+
+export default FeedbackData
