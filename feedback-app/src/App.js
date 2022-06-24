d6257c34d800c8e0121bb7cb5bba332a751300eb
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-24 14:18:48 +0700

    Card Component & Conditional Style

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react'
+
+import { useState  } from 'react'
 import Header from './components/Header'
-import FeedbackItem from './components/FeedbackItem'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
+
 function App()
 {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header/>
       <div className='container'>
-        <FeedbackItem/>
+        <FeedbackList feedback = {feedback}/>
       </div>
     </>
   )
--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import {useState} from 'react'
-
-function FeedbackItem() {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is an example of feedback item')
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(prev);
-            return prev + 1;
-        })
-    }
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+function FeedbackItem({item}) {
   return (
-    <div className="card">
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}</div>
-        <button onClick = {handleClick}>Click</button>
-    </div>
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <div className="text-display">{item.text}</div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
