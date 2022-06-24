d259c5bdb311a89891708e514ca64566f12b0018
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-24 16:36:05 +0700

    FeedbackStats Component & Reactivity

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,6 +3,7 @@ import { useState  } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
 
 function App()
 {
@@ -18,6 +19,7 @@ function App()
     <>
       <Header/>
       <div className='container'>
+        <FeedbackStats feedback = {feedback}/>
         <FeedbackList feedback = {feedback}
         handleDelete = {deleteFeedback}/>
       </div>
