90c10ceac743bc6d70ccb8e5f17e9c1b9cb66057
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-25 10:28:04 +0700

    Rating Select Component

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect"
 
 function FeedbackForm(){
   const[text, setText] = useState('')
@@ -30,7 +31,7 @@ function FeedbackForm(){
     <Card>
         <form>
             <h2>How would you rate your service with us?</h2>
-            {/* todo- rating select component*/}
+            <RatingSelect select={(rating) => setRating(rating)}/>
             <div className="input-group">
                 <input onChange ={handleTextChange} 
                 type="text" 
