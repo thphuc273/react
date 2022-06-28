3a49855970132d01ca89c5a052361b49f551d4ec
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-28 08:55:03 +0700

    Creating Links (v5 & v6)

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 function App()
 {
@@ -41,6 +42,8 @@ function App()
         <Route path="/about" element={<AboutPage/>}></Route>
         {/* <Route path='/about'>This is the about route</Route> */}
       </Routes>
+
+      <AboutIconLink/>
     </div>
     </Router>
     
--- feedback-app/src/pages/AboutPage.jsx
+++ feedback-app/src/pages/AboutPage.jsx
@@ -1,11 +1,16 @@
 import Card from "../components/shared/Card";
-
+import {Link} from 'react-router-dom'
 function AboutPage() {
   return (
     <Card> 
-      <h1>About this project</h1>
-      <p>This is a React app to leave feedback for a product</p>
-      <p>Version 1.0.0</p>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a React app to leave feedback for a product</p>
+        <p>Version 1.0.0</p>
+        <p>
+          <Link to = '/'>Back to home</Link>
+        </p>
+      </div>
     </Card>
   )
 }
