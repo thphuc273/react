7785ec2cd7a36c974e53d6fc6fa287a897cc7856
Phuc Nguyen <thphucnguyen2703@gmail.com>
2022-06-28 09:32:52 +0700

    Nested Routes

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -40,7 +40,7 @@ function App()
         </Route>
         
         <Route path="/about" element={<AboutPage/>}/>
-        <Route path="/post" element={<Post/>}/>
+        <Route path="/post/*" element={<Post/>}/>
       </Routes>
 
       <AboutIconLink/>
